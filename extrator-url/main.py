fc370cbdd731e2c975e48405cdb3c3a1906f3647
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2022-11-04 11:26:16 -0300

    Buscando um valor intermediário do parâmetro de busca

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,6 +1,7 @@
 url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 #print(url)
 
+#SEPARA BASE E OS PARÂMETROS
 indice_interrogacao=url.find('?')
 url_base=url[:indice_interrogacao]
 #print(url_base)
@@ -8,9 +9,11 @@ url_base=url[:indice_interrogacao]
 url_parametros=url[indice_interrogacao+1:]
 print(url_parametros)
 
-paremetro_busca='moedaOrigem'
+#BUSCA O VALOR DO PARÂMETRO DE BUSCA
+paremetro_busca='moedaDestino'
 indice_paremetro=url_parametros.find(paremetro_busca)
 #print(indice_paremetro)
 indice_valor=indice_paremetro+len(paremetro_busca)+1
-valor=url_parametros[indice_valor:]
+indice_e_comercial=url_parametros.find('&')
+valor=url_parametros[indice_valor:indice_e_comercial]
 print(valor)
